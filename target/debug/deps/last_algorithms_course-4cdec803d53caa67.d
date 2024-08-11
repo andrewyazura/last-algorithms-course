7ed28ec5680fbb75202659726b4e75cfb78e04c4
Andrew Yatsura <andrewyazura203@proton.me>
2024-08-11 21:13:04 +0200

    remove target and add .gitignore

--- target/debug/deps/last_algorithms_course-4cdec803d53caa67.d
+++
@@ -1,9 +0,0 @@
-/home/andrewyazura/Documents/last-algorithms-course/target/debug/deps/liblast_algorithms_course-4cdec803d53caa67.rmeta: src/lib.rs src/binary_search.rs src/two_crystal_balls.rs src/bubble_sort.rs src/queue.rs
-
-/home/andrewyazura/Documents/last-algorithms-course/target/debug/deps/last_algorithms_course-4cdec803d53caa67.d: src/lib.rs src/binary_search.rs src/two_crystal_balls.rs src/bubble_sort.rs src/queue.rs
-
-src/lib.rs:
-src/binary_search.rs:
-src/two_crystal_balls.rs:
-src/bubble_sort.rs:
-src/queue.rs:
--- target/debug/deps/last_algorithms_course-b4f26369032df7c9.d
+++
@@ -1,9 +0,0 @@
-/home/andrewyazura/Documents/last-algorithms-course/target/debug/deps/liblast_algorithms_course-b4f26369032df7c9.rmeta: src/lib.rs src/binary_search.rs src/two_crystal_balls.rs src/bubble_sort.rs src/queue.rs
-
-/home/andrewyazura/Documents/last-algorithms-course/target/debug/deps/last_algorithms_course-b4f26369032df7c9.d: src/lib.rs src/binary_search.rs src/two_crystal_balls.rs src/bubble_sort.rs src/queue.rs
-
-src/lib.rs:
-src/binary_search.rs:
-src/two_crystal_balls.rs:
-src/bubble_sort.rs:
-src/queue.rs:
