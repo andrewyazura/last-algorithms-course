698069b315e96b24bae14e932fe8d356c8f46e9d
Andrew Yatsura <andrewyazura203@proton.me>
2024-08-31 03:09:33 +0300

    add second implementation of quick sort

--- quick-sort/quick_sort.go
+++ quick-sort/quick_sort.go
@@ -7,19 +7,49 @@ func QuickSort(array []int) {
 
 	pivot_i := 0
 	pivot := array[pivot_i]
-	i := 1
 
-	for i < len(array) {
+	for i := 1; i < len(array); i++ {
 		if array[i] < pivot {
 			array[pivot_i] = array[i]
 			array[i] = array[pivot_i+1]
 			pivot_i++
 			array[pivot_i] = pivot
 		}
-
-		i++
 	}
 
 	QuickSort(array[:pivot_i])
 	QuickSort(array[pivot_i+1:])
+}
+
+func QuickSort2(array []int) {
+	qs(array, 0, len(array))
+}
+
+func qs(array []int, lo int, hi int) {
+	if lo == hi {
+		return
+	}
+
+	pivot_i := partition(array, lo, hi)
+	qs(array, lo, pivot_i)
+	qs(array, pivot_i+1, hi)
+}
+
+func partition(array []int, lo int, hi int) int {
+	// array[i] where i <= pivot_i is less than pivot_i
+	pivot_i := lo - 1
+	pivot := array[hi-1]
+
+	for i := lo; i < hi-1; i++ {
+		if array[i] <= pivot {
+			pivot_i++
+			array[i], array[pivot_i] = array[pivot_i], array[i]
+		}
+	}
+
+	pivot_i++
+
+	array[hi-1] = array[pivot_i]
+	array[pivot_i] = pivot
+	return pivot_i
 }
--- quick-sort/quick_sort_test.go
+++ quick-sort/quick_sort_test.go
@@ -15,3 +15,13 @@ func TestQuickSort(t *testing.T) {
 		t.Errorf("array is not sorted")
 	}
 }
+
+func TestQuickSort2(t *testing.T) {
+	array := rand.Perm(1_000_000)
+
+	QuickSort2(array)
+
+	if !slices.IsSorted(array) {
+		t.Errorf("array is not sorted")
+	}
+}
